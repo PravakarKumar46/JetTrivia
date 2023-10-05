0761de2d79f888e6b6c686efd539f62676ece4a3
Pravakar <pravakark46@gmail.com>
2023-10-06 00:33:40 +0530

    Added unit test cases for view-model for testing purpose.

---
+++ app/src/test/java/com/example/jettrivia/screens/QuestionViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.example.jettrivia.screens
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class QuestionViewModelTest {
+    @Test
+    fun getTotalNoOfQuestions() {
+        assertEquals(2.2, 2.2, 2.2)
+    }
+}
